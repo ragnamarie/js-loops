ac92d52dbbf5dfa22ec273a63f654258985cf609
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-09 14:55:04 +0200

    add loop

---
+++ js-loops_forof-list/js/index.js
@@ -0,0 +1,24 @@
+console.clear();
+
+const main = document.querySelector('[data-js="main"]');
+
+// Create a list from an array
+
+const ol = document.createElement("ol");
+main.append(ol);
+
+const programmingLanguages = [
+  "JavaScript",
+  "Python",
+  "Java",
+  "C#",
+  "C++",
+  "PHP",
+  "Ruby",
+];
+
+// --v-- write/change code here --v--
+for (const programmingLanguage of programmingLanguages) {
+  console.log(programmingLanguage);
+}
+// --^-- write/change code here --^--
